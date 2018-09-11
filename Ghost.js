4bfc2b0a1fb5161d18263e01e552b113e9693106
Fernanda Rosa dos Santos <43156622+Esnouga@users.noreply.github.com>
2018-09-10 22:07:46 -0300

    Add files via upload

--- Ghost.js
+++ Ghost.js
@@ -1,1 +1,15 @@
-
+var Ghost = function(x, y, cor) {
+//Atributos dinâmicos
+	this.xi = x;//ponto x de inicio
+	this.yi = y;//ponto y de inicio
+	this.x = x;
+	this.y = y;
+	this.cor = cor;
+	this.direcaoAtual = Direcao.naoDefinida;
+	//Métodos dinâmicos
+	this.desenhar = function(ct) {
+		ct.fillStyle = this.cor;
+		ct.fillRect(this.x * largura, this.y * largura,
+		largura, largura);
+	}
+}
