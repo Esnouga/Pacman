310a38ab3a5403d0f3717953beff0f643313d1b4
Fernanda Rosa dos Santos <43156622+Esnouga@users.noreply.github.com>
2018-09-12 09:13:08 -0300

    Update Ghost.js

--- Ghost.js
+++ Ghost.js
@@ -124,3 +124,45 @@ this.mover = function() {
 			break;
 	}
 } //Fim função mover() 
+//Número de movimentos restantes como assustado
+this.assustado = 0;
+this.assustar = function () {
+	this.assustado = 30;
+	switch(this.direcaoAtual) {
+		case Direcao.cima:
+			this.direcaoAtual = Direcao.baixo;
+			break;
+		case Direcao.baixo:
+			this.direcaoAtual = Direcao.cima;
+			break;
+		case Direcao.esquerda:
+			this.direcaoAtual = Direcao.direita;
+			break;
+		case Direcao.direita:
+			this.direcaoAtual = Direcao.esquerda;
+			break;
+	}
+}
+//Quando o Fantasma for devorado
+this.devorado = function () {
+	this.assustado = 0;
+	this.x = this.xi;
+	this.y = this.yi;
+}
+//Adicionar à função mover()
+this.mover = function() {
+	if (this.assustado > 0) {
+		this.assustado--;
+	}
+this.checarDirecoes();
+...
+}
+//Alterar a função para desenhar fantasma assustado
+this.desenhar = function(ct) {
+	if (this.assustado == 0) {
+		ct.fillStyle = this.cor;
+	} else {
+		ct.fillStyle = "rgba(255, 255, 255, 0.8)";
+	}
+	ct.fillRect(this.x * largura, this.y * largura,largura, largura);
+}
